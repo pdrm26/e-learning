4a51f16a9f17b83a128a05f6c679690abcc49573
pedram <peeeedii@gmail.com>
2024-11-29 22:23:29 +0330

    feat: render different type of module

--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models.query import QuerySet
 from django.views.generic.edit import FormView
 from .forms import CourseEnrollForm
 from django.contrib.auth import authenticate, login
@@ -6,6 +7,9 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from courses.models import Course
+from django.views.generic.detail import DetailView
 
 
 class StudentRegistrationView(CreateView):
@@ -16,7 +20,7 @@ class StudentRegistrationView(CreateView):
     def form_valid(self, form):
         result = super().form_valid(form)
         cd = form.cleaned_data
-        user = authenticate(username=cd['username'], password=cd['pasword'])
+        user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
 
@@ -32,3 +36,34 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
     def get_success_url(self) -> str:
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = "students/course/list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = "students/course/detail.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+
+        return context
