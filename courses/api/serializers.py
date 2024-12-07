86fd3058d00797496b87ffdf28340617a5c5d13f
pedram <peeeedii@gmail.com>
2024-12-07 23:29:35 +0330

    feat: add courses serializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Subject
+from courses.models import Subject, Course, Module
 from django.db.models import Count
 
 
@@ -18,3 +18,26 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules'
+        ]
