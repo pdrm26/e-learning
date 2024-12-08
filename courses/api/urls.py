404010864016de6712d8f4767941c7179cade447
pedram <peeeedii@gmail.com>
2024-12-08 23:24:01 +0330

    feat: add join into a enrollment

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -20,5 +20,10 @@ urlpatterns = [
     #     views.SubjectDetailView.as_view(),
     #     name='subject_detail'
     # ),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path(
+        'courses/<pk>/enroll/',
+        views.CourseEnrollView.as_view(),
+        name='course_enroll'
+    ),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -4,7 +4,11 @@ from courses.models import Subject, Course
 from django.db.models import Count
 from courses.api.pagination import StandardPagination
 from rest_framework import viewsets
-
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # class SubjectListView(generics.ListAPIView):
 #     queryset = Subject.objects.annotate(total_courses=Count('courses'))
@@ -26,3 +30,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
