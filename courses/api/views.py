04fb8f7547b40241252e6482e26f006edc37c5d2
pedram <peeeedii@gmail.com>
2024-12-08 23:08:37 +0330

    refactor: use viewsets to handle apis

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,16 +1,28 @@
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.models import Subject, Course
 from django.db.models import Count
 from courses.api.pagination import StandardPagination
+from rest_framework import viewsets
 
 
-class SubjectListView(generics.ListAPIView):
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
