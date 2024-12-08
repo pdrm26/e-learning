b283acbe5794be35e42237e3da1ae885d1430134
pedram <peeeedii@gmail.com>
2024-12-08 23:59:11 +0330

    feat: add the get enroll courses content view

--- courses/api/views.py
+++ courses/api/views.py
@@ -10,6 +10,8 @@ from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 
 # class SubjectListView(generics.ListAPIView):
 #     queryset = Subject.objects.annotate(total_courses=Count('courses'))
@@ -41,6 +43,16 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 # class CourseEnrollView(APIView):
